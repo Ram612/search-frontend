df56693a9f27758bfc70febade08b2c5645d8f0d
Ram <ram@quickminutes.com>
2022-07-10 21:02:32 +0530

    Added axios for api request handling and implemented the solution.

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -1,29 +1,35 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const accessToken = 'QnJxdndgUyKWJOnlqq8UqCHz6k5tCYn9MqCR6Lr4';
+const apiUrl = 'https://api.packt.com/api/v1/products';
+
+const authAxios = axios.create({
+    baseURL: apiUrl,
+    headers: {
+        Authorization: `Bearer ${accessToken}`
+    }
+})
 
 function FilterComponent() {
 
-    const [data, setData] = useState([]);
+    const [data, setData]  = useState([]);
     const [input,setInput] = useState("");
 
-    const fetchBook = async () =>{
-        const response = await fetch("http://127.0.0.1:8000/api/books");
-        //Packt API
-        // const response = await fetch("https://api.packt.com/api/v1/products/{id}?token=QnJxdndgUyKWJOnlqq8UqCHz6k5tCYn9MqCR6Lr4");
-        setData(await response.json());
+    const fetchData = async () =>{
+        // Local API handling
+        // const response = await fetch("http://127.0.0.1:8000/api/books");
+        // const data = await response.json();
+        // setData(data);
+
+        // Todo: Packt API
+        const response = await authAxios.get(``);
+        // console.log(response);
+        setData(response.data.products);
     }
 
-    // const fetchBook = () => {
-    //     fetch("http://127.0.0.1:8000/api/books")
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             setData(data.book);
-    //             console.log(data);
-    //         })
-    //         .catch(err=>console.log(err));
-    // };
-
     useEffect(()=> {
-        fetchBook();
+        fetchData();
     }, []);
 
     return <>
@@ -43,18 +49,18 @@ function FilterComponent() {
             <span className="icon">From Packt</span>
             <ul className="items-unorder">
                 {data
-                    .filter((data) => data.book_title.toLowerCase().includes(input))
+                    .filter((data) => data.title.toLowerCase().includes(input))
                     .map((items)=> {
                     return (
                     <>  
                         <li className="items-list" key={items.id}>
                             <div className="card">
                                 <h3>
-                                    {items.book_title}
+                                    {items.title}
                                 </h3>
                                 
                                 <span className="sub-title">
-                                    ${items.price}
+                                    ${items.authors}
                                 </span>
                             </div>
                         </li>  
